3a6d38a13233ae62f57ed31223687732d9fe02cf
Steven Chien <steven.chien@connect.polyu.hk>
2015-12-20 17:10:56 +0800

    Added student ID check

--- server/checkin.js
+++ server/checkin.js
@@ -1,15 +1,21 @@
 Meteor.methods({
 	'checkin': function(student_id) {
 		var student = Profiles.findOne({ student_id: student_id });
-		Players.upsert({ 
-			id: student._id 
-		}, {
-			id: student._id,
-			sid: student.student_id,
-			f_name: student.f_name,
-			l_name: student.l_name,
-			vote: null
-		});
-		return student;
+		if(student) {
+			var affected = Players.upsert({ 
+				id: student._id 
+			}, {
+				id: student._id,
+				sid: student.student_id,
+				f_name: student.f_name,
+				l_name: student.l_name,
+				vote: null
+			});
+			console.log(affected);
+			return student;
+		}
+		else {
+			throw new Meteor.Error(400,'ID not registered!');
+		}
 	}
 });
