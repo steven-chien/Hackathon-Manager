d3ce6c59195f799daf2736fd11012fc35123dea6
Steven Chien <steven.chien@connect.polyu.hk>
2015-12-21 17:23:11 +0800

    Added user login check for checkin

--- server/checkin.js
+++ server/checkin.js
@@ -1,28 +1,32 @@
 Meteor.methods({
 	'checkin': function(student_id) {
-		var student = Profiles.findOne({ student_id: student_id });
-		if(student) {
-			var affected = Players.upsert({ 
-				id: student._id 
-			}, {
-				id: student._id,
-				sid: student.student_id,
-				f_name: student.f_name,
-				l_name: student.l_name,
-				vote: null,
-				group: false
-			});
-
-			if(affected.insertId) {
-				return { data: student, id: affected.insertId };
+		var userId = Meteor.userId();
+		if(userId) {
+			var student = Profiles.findOne({ student_id: student_id });
+			if(student) {
+				var affected = Players.upsert({ 
+					id: student._id 
+				}, {
+					id: student._id,
+					sid: student.student_id,
+					f_name: student.f_name,
+					l_name: student.l_name,
+					vote: null,
+					group: false
+				});
+	
+				if(affected.insertId) {
+					return { data: student, id: affected.insertId };
+				}
+				else {
+					var id = Players.findOne({ sid: student.student_id });
+					return { data: student, id: id._id };
+				}
 			}
 			else {
-				var id = Players.findOne({ sid: student.student_id });
-				return { data: student, id: id._id };
+				throw new Meteor.Error(400,'ID not registered!');
 			}
-		}
-		else {
-			throw new Meteor.Error(400,'ID not registered!');
+	
 		}
 	}
 });
