0c66fbee4b80a3c5e653ed777909a884369bf666
Steven Chien <steven.chien@connect.polyu.hk>
2016-01-16 19:20:49 +0800

    Instant registration

--- client/register.js
+++ client/register.js
@@ -10,24 +10,48 @@ Template.Register.helpers({
 });
 
 Template.Register.events({
-	'click #read': function(event, template) {
-		Papa.parse(template.find('#csv-file').files[0], {
-			header: true,
-			complete: function(results) {
-				console.log(results);
-				Session.set('parsed_csv', results);
-			},
-			skipEmptyLines: true
-		});
+	'click #read': function(evt, template) {
+		var userId = Meteor.userId();
+		if(userId) {
+			Papa.parse(template.find('#csv-file').files[0], {
+				header: true,
+				complete: function(results) {
+					console.log(results);
+					Session.set('parsed_csv', results);
+				},
+				skipEmptyLines: true
+			});
+		}
 	},
-	'click #clear': function(event) {
+	'click #clear': function(evt) {
 		Session.set('parsed_csv', undefined);
 	},
-	'click #submit': function(event) {
-		var data = Session.get('parsed_csv');
-		if(data) {
-			Meteor.call('register', data.data);
-			Session.set('parsed_csv', undefined);
+	'click #submit': function(evt) {
+		var userId = Meteor.userId();
+		if(userId) {
+			var data = Session.get('parsed_csv');
+			if(data) {
+				Meteor.call('register', data.data);
+				Session.set('parsed_csv', undefined);
+			}
+		}
+	},
+	'click #apply': function(evt) {
+		var userId = Meteor.userId();
+		if(userId) {
+			var student_id = $('#student_id').val();
+			var name = $('#name').val();
+			var year = $('#year').val();
+			var email = $('#email').val();
+			var phone = $('#phone').val();
+			var dept = $('#dept').val();
+			Meteor.call('register', [{ StudentID: student_id, Name: name, Year: year, Email: email, Phone: phone, Department: dept }]);
+			$('#student_id').val('');
+			$('#name').val('');
+			$('#year').val('');
+			$('#email').val('');
+			$('#phone').val('');
+			$('#dept').val('');
 		}
 	}
 });
