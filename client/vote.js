61aa645925c73b9fb17cbe1938a11c5b287c9391
oneonestar <oneonestar@gmail.com>
2016-02-18 04:44:19 +0800

    Verify ID and allow player to vote multiple times.
    Verify player id before display anything.
    Verify player id before voting in backend.
    Remove 'voted' in db.players, now using db.vote table to determine
    whether the player voted or not.
    All the voting records are kept in db.vote table.
    The last voting record will be the final choice of the user.

--- client/vote.js
+++ client/vote.js
@@ -1,24 +1,37 @@
 Template.Vote.rendered = function() {
-	var pattern = new RegExp('\^' + Meteor.absoluteUrl() + 'vote\/(\\w+)$', 'g');
-	var matched = pattern.exec(Router.current().url);
-	Meteor.call('voted', matched[1], function(err, data) {
-		if(data) {
-			Session.set('voted', true);
-		}
-		else {
-			Meteor.subscribe('voteGroups');
-			Session.set('playerId', matched[1]);
-			Session.set('voted', false);
-		}
+        var my_id = Router.current().params._id;
+	Meteor.call('voted', my_id, function(err, data) {
+                //Ensure the id is valid
+                if(err) {
+                    console.log(err);
+	      	    Session.set('valid', false);
+                    return;
+                } else {
+	      	    Session.set('valid', true);
+                }
+                //Check if voted or not
+                Session.set('voted', data);
+                Session.set('playerId', my_id);
+                Meteor.subscribe('voteGroups');
 	});
 };
 
 Template.Vote.helpers({
+        // Ensure the id is valid
+        'valid': function() {
+                return Session.get('valid');
+        },
+        // Return the player has voted or not
 	'voted': function() {
-		var state = Session.get('voted');
-		if(state)
-			return "true";
+		if(typeof Session.get('voted')=='undefined')
+                    return false;
+                return true;
 	},
+        // Return name of the group which the player has voted
+	'votedTo': function() {
+                return Session.get('voted');
+	},
+        // Return the group list
 	'voteGroups': function() {
 		return Groups.find();
 	}
--- server/vote.js
+++ server/vote.js
@@ -1,26 +1,36 @@
 Meteor.methods({
+        // Handle voting
+        // If player id is not valid, throw an error
 	'vote': function(playerId, groupId) {
-		console.log(playerId+' '+groupId);
+		console.log('Vote:'+playerId+' '+groupId);
 		var player = Players.findOne(playerId);
-		if(player.vote==null) {
-			Players.update(playerId, { $set: { vote: true } });
-			var vote = Vote.findOne({ voter: playerId });
-			if(typeof vote != 'undefined') {
-				throw Meteor.Error(500, 'Vote already casted!');
-			}
-			Vote.insert({ voter: playerId, vote: groupId });
-			return true;
-		}
-		else {
-			throw Meteor.Error(500, 'Vote already casted!');
-		}
+
+                // Ensure the id is valid
+                if(player==null) {
+                    throw new Meteor.Error("Invalid playerID", 
+                      "Player ID is not valid.");
+                }
+
+                // Add vote in Vote
+                Vote.insert({ voter: playerId, vote: groupId });
+
+                return true;
 	},
+        // Return the name of the group which the player has voted
+        // If not voted before, return undefined
+        // If player id is not valid, throw an error
 	'voted': function(playerId) {
 		var player = Players.findOne(playerId);
-		console.log(player);
-		if(player.vote==null) {
-			return false;
-		}
-		return true;
+                if(player==null) {
+                    throw new Meteor.Error("Invalid playerID", 
+                      "Player ID is not valid.");
+                }
+                var voted = Vote.findOne({voter:playerId},{sort:{$natural:-1},limit:1});
+                if(typeof voted=='undefined')
+                    return undefined;
+                var voted_groupname = Groups.findOne({_id:voted.vote});
+                if(typeof voted_groupname=='undefined')
+                    return undefined;
+		return voted_groupname.name;
 	}
 })
